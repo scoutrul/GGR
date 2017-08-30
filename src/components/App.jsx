97649e82f66a178bd2546649749486bce27e72e5
Anton Golova <scoutrul@mail.ru>
2017-08-30 22:24:20 +0300

    fetch lang, locations, navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from 'components/routes/home/HomePage';
-import AboutPage from 'components/routes/home/AboutPage';
+import AboutPage from 'components/routes/about/AboutPage';
 
 export default function App() {
   return (
--- src/components/routes/home/HomePage.jsx
+++ src/components/routes/home/HomePage.jsx
@@ -1,52 +1,127 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Backendless from 'backendless';
 
 const APP_ID = '402AB182-EB78-44E3-FF2F-08B037392D00';
 const API_KEY = '2AF3DE22-A4C2-1CAA-FF44-0A4B1FD3FE00';
-Backendless.serverURL = 'https://api.backendless.com';
 Backendless.initApp(APP_ID, API_KEY);
 
-Backendless.UserService.login( 'test@test.tt', 'password', true )
-	.then( function( loggedInUser ) {
-		console.log(`hello User "${loggedInUser.name}"`)
 
-		var locationStorage = Backendless.Data.of( "location" );
-		var whereClause = "name LIKE 'sp'";
-		var queryBuilder = Backendless.DataQueryBuilder.create()
-			.setRelated( ["location_to_language"] )
-			.setWhereClause( whereClause)
-			.setRelationsDepth( 2 )
-			.setProperties("id, name");
+const Language = ({language, onClick}) => (
+	<button onClick={() => onClick(language)}>{language}</button>
+)
+const Locations = ({setLocation, onClick}) => (
+	<button onClick={() => onClick(setLocation)}>{setLocation}</button>
+)
+const Pages = ({setPage, onClick}) => (
+	<button onClick={() => onClick(setPage)}>{setPage}</button>
+)
 
-		locationStorage.find( queryBuilder ).then( function( res ) {
-					console.log(res)
+
+export default class HomePage extends Component {
+	constructor(props) {super(props)}
+	state = {
+			adminUser: null,
+			currentLanguage: "en",
+			languages: [],
+			currentLocation: "Jamaica",
+			locations: [],
+			navigation: [],
+			currentPage: "home",
+		}
+
+	componentDidMount(){
+		this.backendlessGetLocations()
+		this.backendlessUserLogin()
+		this.backendlessNavigation()
+	}
+
+	componentDidUpdate(){
+		console.log(this.state)
+	}
+
+	backendlessNavigation(){
+		Backendless.Data.of("navigation").find()
+			.then((res) => {
+				const navigation = res.map(({name}) => name)
+				this.setState({navigation})
 			})
-			.catch(function(err){
+	}
+
+	backendlessUserLogin(login = 'test@test.tt', password = 'password'){ // TD login form
+		Backendless.UserService.login(login, password, true)
+			.then((loggedInUser) => {
+				this.setState({adminUser: loggedInUser.name})
+			})
+			.catch((err) => {
 				console.log(err)
 			})
-	})
-	.catch(function(err){
-		console.log(err)
-	})
+	}
 
+	backendlessGetLocations(language = this.state.currentLanguage){
+		Backendless.Data.of("language").find()
+			.then((res) => {
+				const languages = res.map(({name}) => name)
+				this.setState({languages})
+			})
+			.then(() => { // get locations depends changeLanguage()
+				const locationStorage = Backendless.Data.of("location");
+				const whereClause = `location_to_language.name = '${language}'`;
+				const queryBuilder = Backendless.DataQueryBuilder.create()
+					.setRelated(["location_to_language"])
+					.setWhereClause(whereClause)
+					.setRelationsDepth(2)
+					.setProperties("name");
+				return locationStorage.find(queryBuilder)
+			})
+			.then((res) => {
+				const locations = res.map(({name}) => name)
+				this.setState({locations})
+			}) 
+			.catch((err) => {
+			})
+	}
 
+	changeLanguage(currentLanguage) {
+		this.setState({currentLanguage},this.backendlessGetLocations(currentLanguage))
+	}
 
+	changeLocation(currentLocation) {
+		this.setState({currentLocation})
+	}
 
-export default function HomePage() {
-	return (
-		<div>
+	changePage(setPage) {
+		this.setState({currentPage: setPage})
+	}
+
+	render() {
+		return (
 			<div>
-				Research result: <br/>
-				Table "Language": 
+				<div>
+					<h1>User is: {this.state.adminUser}</h1>
+				</div>					
+				<div>
+					<h1>Language is: {this.state.currentLanguage}</h1>
+					{this.state.languages.map(language => (
+						<Language key={ language } language={ language } onClick={::this.changeLanguage}/>
+					))}
+
+				</div>
+
+				<div>
+					<h1>Location is: {this.state.currentLocation}</h1>
+					{this.state.locations.map(location => (
+						<Locations key={ location } setLocation={ location } onClick={::this.changeLocation}/>
+					))}
+				</div>
+
+				<div>
+					<h1>Page is: {this.state.currentPage}</h1>
+					{this.state.navigation.map(navigation => (
+						<Pages key={ navigation } setPage={ navigation } onClick={::this.changePage}/>
+					))}
+				</div>
 
 			</div>
-			<h1>
-				Backend + React
-			</h1>
-			<ul>
-				<li>fetch table data</li>
-				<li>edit by input, save</li>
-			</ul>
-		</div>
-	);
+		)
+	}
 }
